a4af0d1730e645456681f5410665bb9a8ac63243
Pankaj <pankaj15297@gmail.com>
2020-01-04 15:01:46 +0530

    Added ( << '.....') and require with new modulefile.rb

---
+++ Ruby/modulefile.rb
@@ -0,0 +1,41 @@
+#====================EXP:::11111===================================
+
+# module Fun
+# 	def Fun.sum(a,b)
+# 		return a+b
+# 	end
+# 	def Fun.sub(a,b)
+# 		return b-a
+# 	end
+# 	def Fun.mul(a,b)
+# 		return a*b
+# 	end
+# 	def Fun.div(a,b)
+# 		return a/b
+# 	end
+# 	def Fun.mod(a,b)
+# 		return a%b
+# 	end
+# end
+
+#====================EXP:::22222===================================
+
+# module Fun
+# 	def sum(a,b)
+# 		return a+b
+# 	end
+# 	def sub(a,b)
+# 		return b-a
+# 	end
+# 	def mul(a,b)
+# 		return a*b
+# 	end
+# 	def div(a,b)
+# 		return a/b
+# 	end
+# 	def mod(a,b)
+# 		return a%b
+# 	end
+# end
+
+#===================================================================
