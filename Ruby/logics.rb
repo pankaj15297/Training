09c2a60941e3c01db8b2eb300775c9d60e9d959a
Pankaj <pankaj15297@gmail.com>
2020-01-22 17:40:48 +0530

    Added a logic to find second/third/... largest number

--- Ruby/logics.rb
+++ Ruby/logics.rb
@@ -31,7 +31,58 @@
 
 # p B = find_1max(a)
 
+#=================Second/Third/... largest number============================
 
+# a = [300,5,2,3,6,7,4,9,8,120,6,25,1,12]
+
+# def find_1max(x)
+# 	max = 0
+# 	for i in 0...x.length do
+# 		for j in 0...x.length do
+# 			if x[j] > max
+# 				max = x[j]
+
+# 			end
+# 		end
+# 	end
+# 	return max
+# end
+
+
+# B = find_1max(a)
+
+# def find_2max(x)
+# 	max = 0
+# 	for i in 0...x.length do
+# 		for j in 0...x.length do
+# 			if x[j] > max and x[j] != B
+# 				max = x[j]
+
+# 			end
+# 		end
+# 	end
+# 	return max
+# end
+
+
+# C = find_2max(a)
+# p C
+
+
+# def find_3max(x)
+# 	max = 0
+# 	for i in 0...x.length do
+# 		for j in 0...x.length do
+# 			if x[j] > max and x[j] != B and x[j] != C
+# 				max = x[j]
+
+# 			end
+# 		end
+# 	end
+# 	return max
+# end
+
+# p find_3max(a)
 
 
 
