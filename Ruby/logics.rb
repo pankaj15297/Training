334e9d434ab981d59fa0b5d0ace361e81bda5864
Pankaj <pankaj15297@gmail.com>
2020-01-22 17:23:29 +0530

    Added a logic to remove #tag words

---
+++ Ruby/logics.rb
@@ -0,0 +1,16 @@
+#================Remove #Tag words=======================
+
+# str = "#This #is Ruby On Rails #framework"
+# arr = str.split(' ')
+# newstr = ""
+# for i in arr do
+#   if i[0] != "#"
+#     newstr += i + " "
+#   end
+# end
+# print newstr
+
+
+
+
+
