284ebed1052ff1b8b89bf760911b386c3f9ab785
Pankaj <pankaj15297@gmail.com>
2020-01-22 17:31:55 +0530

    Added a logic to find largest number

--- Ruby/logics.rb
+++ Ruby/logics.rb
@@ -11,6 +11,27 @@
 # print newstr
 
 
+#=================largest number============================
+
+# a = [300,5,2,3,6,7,4,9,8,120,6,25,1,12]
+
+# def find_1max(x)
+# 	max = 0
+# 	for i in 0...x.length do
+# 		for j in 0...x.length do
+# 			if x[j] > max
+# 				max = x[j]
+
+# 			end
+# 		end
+# 	end
+# 	return max
+# end
+
+
+# p B = find_1max(a)
+
+
 
 
 
