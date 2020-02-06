541965d5e5718564229a92ce41b4311144496551
Pankaj <pankaj15297@gmail.com>
2020-02-06 15:43:00 +0530

    Made all foreign key to not null

--- Rails/blog_post_system/db/migrate/20200206091806_add_ref_to_each_tables.rb
+++ Rails/blog_post_system/db/migrate/20200206091806_add_ref_to_each_tables.rb
@@ -6,5 +6,12 @@ class AddRefToEachTables < ActiveRecord::Migration[6.0]
   	add_reference :likes, :post, foreign_key: true
   	add_reference :comments, :user, foreign_key: true
   	add_reference :comments, :post, foreign_key: true
+
+  	change_column_null :posts, :user_id, false
+  	change_column_null :posts, :category_id, false
+  	change_column_null :likes, :user_id, false
+  	change_column_null :likes, :post_id, false
+  	change_column_null :comments, :user_id, false
+  	change_column_null :comments, :post_id, false
   end
 end
