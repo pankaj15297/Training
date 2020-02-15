43a97c65b6c24aae4743a89d5dea6ee1438b5c90
Pankaj <pankaj15297@gmail.com>
2020-02-15 18:26:03 +0530

    Added polymorphic association

--- Rails/blog_post_system/app/models/post.rb
+++ Rails/blog_post_system/app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  has_many :post_comments, class_name: "Comment", foreign_key: "post_id", dependent: :destroy
   has_many :post_likes, class_name: "Like", foreign_key: "post_id", dependent: :destroy
+  has_many :liked_users, through: :post_likes, source: :user
+  has_many :post_comments, class_name: "Comment", foreign_key: "post_id", dependent: :destroy
+  has_many :commented_users, through: :post_comments, source: :user
+  has_many :post_pictures, as: :imageable, class_name: "Picture", foreign_key: "imageable_id"
 end
--- Rails/blog_post_system/app/models/user.rb
+++ Rails/blog_post_system/app/models/user.rb
@@ -1,5 +1,19 @@
 class User < ApplicationRecord
-  has_many :posts, class_name: "Post", foreign_key: "user_id", dependent: :destroy
+  has_many :user_posts, -> { order('title asc') }, class_name: "Post", foreign_key: "user_id", dependent: :destroy
   has_many :user_likes, class_name: "Like", foreign_key: "user_id", dependent: :destroy
+  has_many :liked_posts, through: :user_likes, source: :post
   has_many :user_comments, class_name: "Comment", foreign_key: "user_id", dependent: :destroy
+  has_many :commented_posts, through: :user_comments, source: :post
+  has_many :user_pictures, as: :imageable, class_name: "Picture", foreign_key: "imageable_id"
+
+  validates :name, presence: true
+
+
+
+#   after_create :fun
+
+# def fun
+#   puts "Hello"
+# end
 end
+
