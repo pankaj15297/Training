743becc77ced65a5f569e2547be14128492fb3eb
Pankaj <pankaj15297@gmail.com>
2020-02-21 18:48:29 +0530

    Added actions and methods to controller

--- Rails/blog_post_system/app/controllers/users_controller.rb
+++ Rails/blog_post_system/app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
 	def index
+		@users = User.all
 	end
-	
+
 	def new
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def create
+    @user = User.new(user_params)
+    if @user.save
+    	redirect_to @user
+    else
+    	render 'new'
+    end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:name, :contact, :paid, :address)
 	end
 end
