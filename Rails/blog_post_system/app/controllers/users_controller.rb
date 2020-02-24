8340905db7b291df8d25cac53ed2014a9939ef6f
Pankaj <pankaj15297@gmail.com>
2020-02-24 19:48:25 +0530

    Added method to edit and update user

--- Rails/blog_post_system/app/controllers/users_controller.rb
+++ Rails/blog_post_system/app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
 		@user = User.new()
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -18,6 +22,15 @@ class UsersController < ApplicationController
     else
     	render 'new'
     end
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			redirect_to @user
+		else
+			render 'edit'
+		end
 	end
 
 	private
