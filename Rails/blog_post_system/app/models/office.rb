8723cc29ce276421b0b92fac57e7cb11c9f4ca7f
Pankaj <pankaj15297@gmail.com>
2020-02-25 16:24:45 +0530

    Deleted model and migration

--- Rails/blog_post_system/app/models/office.rb
+++
@@ -1,6 +0,0 @@
-class Office < ApplicationRecord
-  has_and_belongs_to_many :office_users, class_name: "User"
-  validates :pincode, numericality: { only_integer: true }, length: { is: 6, message: "Pincode should be 6 digits" }, allow_blank: true
-  validates :name, presence: true, uniqueness: true, on: :update
-  validates :address, length: { in: 10..100 }
-end
