0ef547e363cb03ba085b4120dcc6400a309e3cc4
Pankaj <pankaj15297@gmail.com>
2020-02-28 16:02:43 +0530

    Using namespace to make admin

--- Rails/blog_post_system/config/routes.rb
+++ Rails/blog_post_system/config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
   # get '/user/new', to: 'users#new', as: 'new_user'
-  resources :users, except: [:show, :edit]
-  get '/users/:id', to: 'users#show', as: 'show_user'
-  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  namespace  :admin do
+    resources :users
+  end
+  # get '/users/:id', to: 'users#show', as: 'show_user'
+  # get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
   get '*path', :to => 'application#routing_error'
 end
