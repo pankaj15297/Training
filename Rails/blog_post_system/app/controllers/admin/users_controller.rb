9fe71e015485085fef574df80b5dcf68eb66ec7c
Pankaj <pankaj15297@gmail.com>
2020-02-28 16:10:54 +0530

    Added admin folder in controller

---
+++ Rails/blog_post_system/app/controllers/admin/users_controller.rb
@@ -0,0 +1,59 @@
+class Admin::UsersController < ApplicationController
+  protect_from_forgery except: [:create, :edit]
+  def index
+    if User.any?
+      @users = User.all
+    else
+      render html: helpers.tag.strong("No Record Found")
+    end
+  end
+
+ def new
+   @user = User.new()
+ end
+ 
+  def edit
+    begin
+      @user = User.find(params[:id])
+    rescue => e
+      render html: helpers.tag.strong("#{e.message}")
+    end
+  end
+
+  def show
+    begin
+      @user = User.find(params[:id])
+    rescue => e
+      render html: helpers.tag.strong("#{e.message}")
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_user_path(@user.id)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:firstname, :lastname, :email, :password, :contact, :paid, :address)
+  end
+end
