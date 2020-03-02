02e9d76dea711b1106d5dad0f7c5f31a3c46dc5d
Pankaj <pankaj15297@gmail.com>
2020-03-02 16:20:40 +0530

    Defined New Show Edit Index actions

--- Rails/blog_post_system/app/controllers/admin/posts_controller.rb
+++ Rails/blog_post_system/app/controllers/admin/posts_controller.rb
@@ -2,15 +2,31 @@ class Admin::PostsController < ApplicationController
   protect_from_forgery except: [:create, :edit]
   layout "posts"
   def index
+  	if Post.present?
+  	  @posts = Post.all
+  	else
+      render html: helpers.tag.strong("No Record Found")
+    end
   end
 
   def new
+    @post = Post.new()
   end
 
   def edit
+    begin
+      @post = Post.find(params[:id])
+    rescue => e
+      render html: helpers.tag.strong("#{e.message}")
+    end
   end
 
   def show
+    begin
+      @post = Post.find(params[:id])
+    rescue => e
+      render html: helpers.tag.strong("#{e.message}")
+    end
   end
 
   def create
