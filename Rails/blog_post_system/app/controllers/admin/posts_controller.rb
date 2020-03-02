04156a2fcbb6e9c45e942846b2dac423627cea33
Pankaj <pankaj15297@gmail.com>
2020-03-02 16:01:13 +0530

    Added all actions of posts

--- Rails/blog_post_system/app/controllers/admin/posts_controller.rb
+++ Rails/blog_post_system/app/controllers/admin/posts_controller.rb
@@ -1,3 +1,28 @@
 class Admin::PostsController < ApplicationController
   protect_from_forgery except: [:create, :edit]
+  layout "posts"
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def post_params
+  end
 end
