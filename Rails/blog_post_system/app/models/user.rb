186f02bec4e6652c7bceafaf058b7dc7a5a5cd32
Pankaj <pankaj15297@gmail.com>
2020-03-04 19:10:11 +0530

    comment the callbacks

--- Rails/blog_post_system/app/models/user.rb
+++ Rails/blog_post_system/app/models/user.rb
@@ -1,13 +1,14 @@
 class MyValidator < ActiveModel::Validator
-  def validates(record)
+  def validate(record)
     if record.firstname == "Delhi"
       record.errors[:base] << "This is not a name"
+      puts "name is delhi"
     end
   end
 end
 
 class User < ApplicationRecord
-  before_save :firstname_capitalization, :lastname_capitalization, :paid_capitalization, :address_capitalization, :email_downcase
+  # before_save :firstname_capitalization, :lastname_capitalization, :paid_capitalization, :address_capitalization, :email_downcase
   
   has_many :user_posts, -> { order('title asc') }, class_name: "Post", foreign_key: "user_id", dependent: :destroy
   
@@ -26,53 +27,64 @@ class User < ApplicationRecord
   validates :contact, uniqueness: true, presence: true, numericality: true, length: { is: 10 }
   validates :email, uniqueness: true, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
     message: "not valid" }
+    
   validates_with MyValidator
 
-  scope :paid, -> (args){ where ("paid = ?"), args }
+  # def update_with_conflict_validation(*args)
+  #   #update_attributes(*args)
+  # rescue ActiveRecord::StaleObjectError
+  #   errors.add :base, "This record change while you were editing."
+  #   changes.each do |name, value|
+  #     errors.add name, "was #{values.first}"
+  #   end
+  #   false
+  # end
 
-  after_initialize do |user|
-    puts "You have initialized an object!"
-  end
+  scope :paid, -> (args){ where("paid = ?", args) if args.present? }
 
-  after_find do |user|
-    puts "You have found an object!"
-  end
+  # after_initialize do |user|
+  #   puts "You have initialized an object!"
+  # end
 
-  after_touch do |user|
-    puts "You have touched an object"
-  end
+  # after_find do |user|
+  #   puts "You have found an object!"
+  # end
+
+  # after_touch do |user|
+  #   puts "You have touched an object"
+  # end
   
-  def firstname_capitalization
-    word = firstname.split(' ')
-    word.each do |w|
-      w.capitalize!
-    end
-    self.firstname = word.join(' ')
-  end
+  # def firstname_capitalization
+  #   word = firstname.split(' ')
+  #   word.each do |w|
+  #     w.capitalize!
+  #   end
+  #   self.firstname = word.join(' ')
+  # end
 
-  def lastname_capitalization
-    word = lastname.split(' ')
-    word.each do |w|
-      w.capitalize!
-    end
-    self.lastname = word.join(' ')
-  end
+  # def lastname_capitalization
+  #   word = lastname.split(' ')
+  #   word.each do |w|
+  #     w.capitalize!
+  #   end
+  #   self.lastname = word.join(' ')
+  # end
 
-  def paid_capitalization
-    self.paid = paid.capitalize
-  end
+  # def paid_capitalization
+  #   self.paid = paid.capitalize
+  # end
   
-  def address_capitalization
-    word = address.split(' ')
-    word.each do |w|
-      w.capitalize!
-    end
-    self.address = word.join(' ')
-  end
+  # def address_capitalization
+  #   word = address.split(' ')
+  #   word.each do |w|
+  #     w.capitalize!
+  #   end
+  #   self.address = word.join(' ')
+  # end
 
-  def email_downcase
-    self.email = email.downcase
-  end
+  # def email_downcase
+  #   self.email = email.downcase
+  # end
 
 end
 
