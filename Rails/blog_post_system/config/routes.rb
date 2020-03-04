8a46615a98ee0f785cdaeafaa9ce4cb7e4b33cab
Pankaj <pankaj15297@gmail.com>
2020-03-04 18:39:03 +0530

    Added routes

--- Rails/blog_post_system/config/routes.rb
+++ Rails/blog_post_system/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
-  # get '/user/new', to: 'users#new', as: 'new_user'
+  # get 'admin/:id/users', to: 'admin/users#soft_delete', as: 'admin_soft_delete'
+  
   namespace  :admin do
     resources :users, :posts
   end
-  get 'admin/:id/users', to: 'admin/users#soft_delete', as: 'admin_soft_delete'
-  # get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  patch 'admin/users/:id/soft_delete', to: "admin/users#soft_delete", as: "user_soft_delete"
   get '*path', :to => 'application#routing_error'
 end
