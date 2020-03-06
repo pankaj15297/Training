32ef98dbde81e706a45b252fec8ee11464e34545
Pankaj <pankaj15297@gmail.com>
2020-03-06 19:03:58 +0530

    initialize actions

--- Rails/blog_post_system/app/controllers/pictures_controller.rb
+++ Rails/blog_post_system/app/controllers/pictures_controller.rb
@@ -1,18 +1,18 @@
 class PicturesController < ApplicationController
   def index
-  	
+    @pictures = Picture.all  	
   end
 
   def new
-  	
+    @picture = Picture.new
   end
 
   def edit
-  	
+    @picture = Picture.find(params[:id])
   end
 
   def show
-  	
+    @picture = Picture.find(params[:id])
   end
 
   def create
