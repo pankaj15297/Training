740a9a5cddd716c98e21141ade3e605b933361b4
Pankaj <pankaj15297@gmail.com>
2020-03-06 16:49:40 +0530

    Added 7 actions

--- Rails/blog_post_system/app/controllers/pictures_controller.rb
+++ Rails/blog_post_system/app/controllers/pictures_controller.rb
@@ -1,2 +1,29 @@
 class PicturesController < ApplicationController
+  def index
+  	
+  end
+
+  def new
+  	
+  end
+
+  def edit
+  	
+  end
+
+  def show
+  	
+  end
+
+  def create
+  	
+  end
+
+  def update
+  	
+  end
+
+  def destroy
+  	
+  end
 end
