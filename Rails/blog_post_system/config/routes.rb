ebe53fcc481bf7ee3ec5448b6391893bbde478f1
Pankaj <pankaj15297@gmail.com>
2020-03-07 13:14:20 +0530

    added routes for pictures resource

--- Rails/blog_post_system/config/routes.rb
+++ Rails/blog_post_system/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # get 'admin/:id/users', to: 'admin/users#soft_delete', as: 'admin_soft_delete'
   
   namespace  :admin do
-    resources :users, :posts
+    resources :users, :posts, :pictures
   end
   patch 'admin/users/:id/soft_delete', to: "admin/users#soft_delete", as: "user_soft_delete"
   get '*path', :to => 'application#routing_error'
