db04b7c7d788d57c76c0158ab48ed9c772419243
Pankaj <pankaj15297@gmail.com>
2020-03-11 17:01:07 +0530

    fixed the routes

--- Rails/blog_post_system/config/routes.rb
+++ Rails/blog_post_system/config/routes.rb
@@ -3,13 +3,17 @@ Rails.application.routes.draw do
   root 'welcome#index'
   # get 'admin/:id/users', to: 'admin/users#soft_delete', as: 'admin_soft_delete'
   
-  namespace  :admin do
-    namespace :api do
-      namespace :v1 do
-        resources :users, :posts, :pictures
-      end
-    end
+namespace :admin do
+  resources :users, :posts, :pictures
+end
+
+
+namespace :api do
+  namespace :v1 do
+    resources :users, :posts, :pictures
   end
+end
+
   patch 'admin/users/:id/soft_delete', to: "admin/users#soft_delete", as: "user_soft_delete"
   get '*path', :to => 'application#routing_error'
 end
