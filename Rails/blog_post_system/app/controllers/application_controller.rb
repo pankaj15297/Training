3d21e2b43f97a98768dbb0e85bd278334632dcf5
Pankaj <pankaj15297@gmail.com>
2020-03-11 17:58:08 +0530

    modified

--- Rails/blog_post_system/app/controllers/application_controller.rb
+++ Rails/blog_post_system/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
-class ApplicationController < ActionController::API
-  # protect_from_forgery
+class ApplicationController < ActionController::Base
+  protect_from_forgery
   def routing_error
   	render file: "#{Rails.root}/public/404.html"
   end
