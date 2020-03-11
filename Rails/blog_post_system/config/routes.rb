be844f9e89997dc36129e197c9331c1b5a90a9e8
Pankaj <pankaj15297@gmail.com>
2020-03-11 14:27:13 +0530

    changed routes

--- Rails/blog_post_system/config/routes.rb
+++ Rails/blog_post_system/config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   # get 'admin/:id/users', to: 'admin/users#soft_delete', as: 'admin_soft_delete'
   
   namespace  :admin do
-    resources :users, :posts, :pictures
+    namespace :api do
+      namespace :v1 do
+        resources :users, :posts, :pictures
+      end
+    end
   end
   patch 'admin/users/:id/soft_delete', to: "admin/users#soft_delete", as: "user_soft_delete"
   get '*path', :to => 'application#routing_error'
