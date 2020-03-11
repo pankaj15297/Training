ee6bcbff302ee175e21b25500fecfe3db5534170
Pankaj <pankaj15297@gmail.com>
2020-03-11 16:07:12 +0530

    added render json for API on CURD operation

--- Rails/blog_post_system/app/controllers/admin/api/v1/users_controller.rb
+++ Rails/blog_post_system/app/controllers/admin/api/v1/users_controller.rb
@@ -1,12 +1,12 @@
 class Admin::Api::V1::UsersController < ApplicationController
-  protect_from_forgery except: [:create, :edit]
-  layout "users"
+  # protect_from_forgery except: [:create, :edit]
+  # layout "users"
   def index
     # binding.pry
     @page = params.fetch(:page, 0).to_i
     @users = User.order(created_at: :desc).limit(4).offset(@page*4)
     @records = User.count
-    render json: @users
+    render json: {status: 'Success', message: 'Loaded users', data: @users}, status: :ok
     # @users = User.paginate(:per_page => 2, :page => params[:page])
     # @users = User.all.order(:created_at)
     # @users = User.where(is_deleted: false)
@@ -30,31 +30,37 @@ class Admin::Api::V1::UsersController < ApplicationController
     rescue => e
       render html: helpers.tag.strong("#{e.message}")
     end
+    render json: {status: 'Success', message: 'Loaded user', data: @user}, status: :ok
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to admin_user_path(@user.id), notice: "User was successfully created."
+      render json: {status: 'Success', message: 'Saved user', data: @user}, status: :ok
+      # redirect_to admin_user_path(@user.id), notice: "User was successfully created."
       # format.html {redirect_to admin_user_path(@user.id), notice: 'User was successfully created.'}
     else
-      render 'new'
+      # render 'new'
+      render json: {status: 'Error', message: 'User not saved', data: @user.errors}, status: :unprocessable_entity
     end
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to admin_user_path(@user.id)
+      # redirect_to admin_user_path(@user.id)
+      render json: {status: 'Success', message: 'Updated user', data: @user}, status: :ok
     else
-      render 'edit'
+      # render 'edit'
+      render json: {status: 'Success', message: 'User not updated', data: @user.errors}, status: :unprocessable_entity
     end
   end
 
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to admin_users_path
+    # redirect_to admin_users_path
+    render json: {status: 'Success', message: 'Deleted user', data: @user}, status: :ok
   end
 
   def soft_delete
